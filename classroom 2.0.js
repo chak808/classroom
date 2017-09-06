ce3ec9b5401e0abf0736fde4308e8799beb8e707
chak808 <30875250+chak808@users.noreply.github.com>
2017-09-06 09:15:10 -1000

    Create classroom 2.0.js

---
+++ classroom 2.0.js
@@ -0,0 +1,19 @@
+
+var classroom = {
+roomnumber:"F103",
+students:18,
+};
+var x = prompt("roomnumber, or students?");
+var lowerinput = x.toLowerCase();
+if (lowerinput==="roomnumber"||lowerinput==="students")
+{window.alert(classroom[lowerinput]);}
+else {
+for(i = 0; i < 3; i++) {
+var x = prompt("roomnumber, or students?");
+var lowerinput = x.toLowerCase();
+if (lowerinput==="roomnumber"||lowerinput==="students")
+{
+i = 3;
+window.alert(classroom[lowerinput]);}
+}
+}
